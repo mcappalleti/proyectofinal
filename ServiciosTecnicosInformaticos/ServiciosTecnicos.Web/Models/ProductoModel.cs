c6fd519eedcf550d8ac4107589be4231bd61e1da
mcappalleti <josselinemari@hotmail.es>
2019-07-28 21:20:27 -0500

    Agregamos lista de productos

---
+++ ServiciosTecnicosInformaticos/ServiciosTecnicos.Web/Models/ProductoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiciosTecnicos.Web.Models
+{
+    public class ProductoModel
+    {
+
+        public  int Id { get; set; }
+        public  string Descripcion { get; set; }
+       /* public double precio { get; set; }*/
+
+
+
+    }
+}
